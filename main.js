6e2159a0be54e5916657cd692ed9c5877cb82685
thp728 <thp728@gmail.com>
2023-07-14 02:45:56 +0530

    Updated code formatting

--- main.js
+++ main.js
@@ -13,146 +13,173 @@ let asteroids = [];
 let score = 0;
 let lives = 3;
 
-document.addEventListener('DOMContentLoaded', SetupCanvas);
+document.addEventListener("DOMContentLoaded", SetupCanvas);
 
 function SetupCanvas() {
-    canvas = document.getElementById('my-canvas');
-    ctx = canvas.getContext('2d');
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0,0, canvas.width, canvas.height);
+  canvas = document.getElementById("my-canvas");
+  ctx = canvas.getContext("2d");
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    for(let i=0; i<8; i++) {
-        asteroids.push(new Asteroid());
+  for (let i = 0; i < 8; i++) {
+    asteroids.push(new Asteroid());
+  }
+
+  document.body.addEventListener("keydown", function (e) {
+    keys[e.key] = true;
+  });
+  document.body.addEventListener("keyup", function (e) {
+    keys[e.key] = false;
+    if (e.key === " ") {
+      bullets.push(new Bullet(ship.angle));
     }
-
-    document.body.addEventListener("keydown", function(e){
-        keys[e.key] = true;
-    });
-    document.body.addEventListener("keyup", function(e){
-        keys[e.key] = false;
-        if(e.key === " ") {
-            bullets.push(new Bullet(ship.angle));
-        }
-    });
-    Render();
+  });
+  Render();
 }
 
 function CircleCollisions(p1x, p1y, r1, p2x, p2y, r2) {
-    let radiusSum;
-    let xDiff;
-    let yDiff;
-    radiusSum = r1 + r2;
-    xDiff = p1x - p2x;
-    yDiff = p1y - p2y;
+  let radiusSum;
+  let xDiff;
+  let yDiff;
+  radiusSum = r1 + r2;
+  xDiff = p1x - p2x;
+  yDiff = p1y - p2y;
 
-    if(radiusSum > Math.sqrt((xDiff*xDiff) + (yDiff*yDiff)) ) {
-        return true;
-    } else {
-        return false;
-    }
+  if (radiusSum > Math.sqrt(xDiff * xDiff + yDiff * yDiff)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function DrawLifeShips() {
-    let startX = 1350;
-    let startY = 10;
-    let points = [[9,9], [-9,9]];
-    ctx.strokeStyle = 'white';
+  let startX = 1350;
+  let startY = 10;
+  let points = [
+    [9, 9],
+    [-9, 9],
+  ];
+  ctx.strokeStyle = "white";
 
-    for(let i=0; i<lives; i++) {
-        ctx.beginPath();
-        ctx.moveTo(startX, startY);
+  for (let i = 0; i < lives; i++) {
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
 
-        for(let j=0; j<points.length; j++) {
-            ctx.lineTo(startX+points[j][0], startY + points[j][1]);
-        }
-        ctx.closePath();
-        ctx.stroke();
-        startX -= 30;
+    for (let j = 0; j < points.length; j++) {
+      ctx.lineTo(startX + points[j][0], startY + points[j][1]);
     }
+    ctx.closePath();
+    ctx.stroke();
+    startX -= 30;
+  }
 }
 
-// Update positions of all shapes and also draw them 
+// Update positions of all shapes and also draw them
 function Render() {
-    ship.movingForward = (keys["w"]); // W key
-    // D key
-    if(keys["d"]) {
-        ship.Rotate(1);
-    }
-    // A key
-    if(keys["a"]) {
-        ship.Rotate(-1);
-    }
+  ship.movingForward = keys["w"]; // W key
+  // D key
+  if (keys["d"]) {
+    ship.Rotate(1);
+  }
+  // A key
+  if (keys["a"]) {
+    ship.Rotate(-1);
+  }
 
-    ctx.clearRect(0,0, canvasWidth, canvasHeight);
-    ctx.fillStyle = 'white';
-    ctx.font = '21px Arial';
-    ctx.fillText('SCORE: '+ score.toString(), 20, 35);
-    if(lives<=0) {
-        ship.visible = false;
-        ctx.fillStyle = 'white';
-        ctx.font = '50px Arial';
-        ctx.fillText('GAME OVER', canvasWidth/2 - 150, canvasHeight/2);
-    }
-    DrawLifeShips();
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  ctx.fillStyle = "white";
+  ctx.font = "21px Arial";
+  ctx.fillText("SCORE: " + score.toString(), 20, 35);
+  if (lives <= 0) {
+    ship.visible = false;
+    ctx.fillStyle = "white";
+    ctx.font = "50px Arial";
+    ctx.fillText("GAME OVER", canvasWidth / 2 - 150, canvasHeight / 2);
+  }
+  DrawLifeShips();
 
-    if(asteroids.length !== 0) {
-        for(let k=0; k<asteroids.length; k++) {
-            if(CircleCollisions(ship.x, ship.y, 11, asteroids[k].x, asteroids[k].y, asteroids[k].collisionRadius)) {
-                ship.x = canvasWidth/2;
-                ship.y = canvasHeight/2;
-                ship.velX = 0;
-                ship.velY = 0;
-                lives -= 1;
-            }
+  if (asteroids.length !== 0) {
+    for (let k = 0; k < asteroids.length; k++) {
+      if (
+        CircleCollisions(
+          ship.x,
+          ship.y,
+          11,
+          asteroids[k].x,
+          asteroids[k].y,
+          asteroids[k].collisionRadius
+        )
+      ) {
+        ship.x = canvasWidth / 2;
+        ship.y = canvasHeight / 2;
+        ship.velX = 0;
+        ship.velY = 0;
+        lives -= 1;
+      }
+    }
+  }
+
+  if (asteroids.length !== 0 && bullets.length !== 0) {
+    loop1: for (let l = 0; l < asteroids.length; l++) {
+      for (let m = 0; m < bullets.length; m++) {
+        if (
+          CircleCollisions(
+            bullets[m].x,
+            bullets[m].y,
+            3,
+            asteroids[l].x,
+            asteroids[l].y,
+            asteroids[l].collisionRadius
+          )
+        ) {
+          if (asteroids[l].level === 1) {
+            asteroids.push(
+              new Asteroid(asteroids[l].x - 5, asteroids[l].y - 5, 25, 2, 22)
+            );
+            asteroids.push(
+              new Asteroid(asteroids[l].x + 5, asteroids[l].y + 5, 25, 2, 22)
+            );
+          } else if (asteroids[l].level === 2) {
+            asteroids.push(
+              new Asteroid(asteroids[l].x - 5, asteroids[l].y - 5, 15, 3, 12)
+            );
+            asteroids.push(
+              new Asteroid(asteroids[l].x + 5, asteroids[l].y + 5, 15, 3, 12)
+            );
+          }
+
+          // Delete colliding asteroid and bullet
+          asteroids.splice(l, 1);
+          bullets.splice(m, 1);
+
+          score += 10;
+          break loop1;
         }
+      }
     }
+  }
 
-    if(asteroids.length !== 0 && bullets.length !== 0) {
-loop1:
-        for(let l=0; l<asteroids.length; l++) {
-            for(let m=0; m<bullets.length; m++) {
-                if(CircleCollisions(bullets[m].x, bullets[m].y, 3, asteroids[l].x, asteroids[l].y, asteroids[l].collisionRadius)) {
-                    if(asteroids[l].level === 1) {
-                        asteroids.push(new Asteroid(asteroids[l].x - 5, asteroids[l].y - 5, 25, 2, 22));
-                        asteroids.push(new Asteroid(asteroids[l].x + 5, asteroids[l].y + 5, 25, 2, 22));
-                    } else if(asteroids[l].level === 2) {
-                        asteroids.push(new Asteroid(asteroids[l].x - 5, asteroids[l].y - 5, 15, 3, 12));
-                        asteroids.push(new Asteroid(asteroids[l].x + 5, asteroids[l].y + 5, 15, 3, 12));
-                    }
+  if (ship.visible) {
+    ship.Update();
+    ship.Draw();
+  }
 
-                    // Delete colliding asteroid and bullet
-                    asteroids.splice(l, 1);
-                    bullets.splice(m, 1);
-
-                    score += 10;
-                    break loop1;
-                }
-            }
-        }
+  if (bullets.length !== 0) {
+    for (let i = 0; i < bullets.length; i++) {
+      bullets[i].Update();
+      bullets[i].Draw();
     }
+  }
 
-    if(ship.visible) {
-        ship.Update();
-        ship.Draw();
+  if (asteroids.length !== 0) {
+    for (let j = 0; j < asteroids.length; j++) {
+      asteroids[j].Update();
+      asteroids[j].Draw(j);
     }
+  }
 
-
-    if(bullets.length !== 0) {
-        for(let i=0; i<bullets.length; i++) {
-            bullets[i].Update();
-            bullets[i].Draw();
-        }
-    }
-
-    if(asteroids.length !== 0) {
-        for(let j=0; j<asteroids.length; j++) {
-            asteroids[j].Update();
-            asteroids[j].Draw(j);
-        }
-    }
-
-    // Repeatedly calls Render()
-    requestAnimationFrame(Render);
+  // Repeatedly calls Render()
+  requestAnimationFrame(Render);
 }
